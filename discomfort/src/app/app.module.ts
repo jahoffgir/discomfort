a6edfad23a9d5f741c444e597c5d6086a0736748
Juri Kiin <kiin.juri@gmail.com>
2019-02-16 18:48:42 -0500

    - Added Timeline component

--- discomfort/src/app/app.module.ts
+++ discomfort/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
 import { MainComponent } from './main/main.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     AppWelcomeComponent,
-    MainComponent
+    MainComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
